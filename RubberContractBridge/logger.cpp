d23d32c7760b9aff54592c3003e77da8874300d4
Christiaan Wagner <u04503237@tuks.co.za>
2020-09-27 13:56:08 +0200

    Added Logger declarations in .h and .cpp

--- RubberContractBridge/logger.cpp
+++ RubberContractBridge/logger.cpp
@@ -4,3 +4,23 @@ Logger::Logger(QObject *parent) : QObject(parent)
 {
 
 }
+
+void Logger::enableLog()
+{
+
+}
+
+void Logger::disableLog()
+{
+
+}
+
+bool Logger::isLogEnabled() const
+{
+    return enableLogging;
+}
+
+void Logger::log(QString context, QString msg)
+{
+
+}
--- RubberContractBridge/main.cpp
+++ RubberContractBridge/main.cpp
@@ -1,10 +1,16 @@
 #include "mainwindow.h"
 #include "clientgui.h"
 #include "servergui.h"
+#include "logger.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
+    // Test logger class
+    Logger testLog;
+
+
+    //
     QApplication rcbApplication(argc, argv);
     MainWindow entryWindow;
     entryWindow.show();

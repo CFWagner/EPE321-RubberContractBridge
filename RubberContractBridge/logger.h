ced099841b86b51921a5c0168e591a441e03f32f
Christiaan Wagner <u04503237@tuks.co.za>
2020-09-27 13:56:08 +0200

    Added Logger declarations in .h and .cpp

--- RubberContractBridge/logger.h
+++ RubberContractBridge/logger.h
@@ -8,8 +8,19 @@ class Logger : public QObject
     Q_OBJECT
 public:
     explicit Logger(QObject *parent = nullptr);
+    void enableLog();
+    void disableLog();
+    bool isLogEnabled() const;
 
 signals:
+    void sendLog (QString context, QString msg) const;
+
+
+private slots:
+    void log(QString context, QString msg);
+
+private:
+    bool enableLogging;
 
 };
 

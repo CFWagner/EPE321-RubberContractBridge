d357ac66b50ee47eb71367c06d06b78487ac950e
Christiaan Wagner <u04503237@tuks.co.za>
2020-09-27 18:11:07 +0200

    Added declarations in .h and .cpp files. Inheritace should be implemented. Enums and external classes should be added.

--- RubberContractBridge/playernetwork.h
+++ RubberContractBridge/playernetwork.h
@@ -19,6 +19,7 @@ public:
     enum Bid{};
     enum Move{};
     enum Team{};
+    class Card{};
     class PlayerGameState{};
     enum PlayerPosition{};
 
@@ -29,23 +30,36 @@ public:
     void notifyMoveRejected(QString reason);
     void message(QString source, QString msg);
     void gameTerminated(QString reason);
-    QString getPlayerName();
-    PlayerPosition getPosition();
-    Team getTeam();
+    QString getPlayerName() const;
+    PlayerPosition getPosition() const;
+    Team getTeam() const;
     void setPosition(PlayerPosition position);
-    QString toString();
+    QString toString() const;
+
 private slots:
+    void rxAll();
+    void pingClient();
 
 signals:
+    void bidSelected(Bid bid);
+    void moveSelected(Card card);
+    void messageGenerated(QString msg);
+    void clientDisconnected();
 
 private:
+    void txAll(QJsonObject data);
+    void rxBidSelected(QJsonObject bidObj);
+    void rxMoveSelected(QJsonObject moveObj);
+    void rxMessage(QJsonObject msgObj);
+    void rxPingReturn();
+    void internalClientDisconnected(); //Notice the name change between this and the signal's name in the Group design doc.
+
     QTcpSocket* clientSoc;
     QTimer* keepAlive;
     bool aliveFlag;
     QDataStream in;
     QDataStream out;
-
-
+    // playerName and playerPosition is declared in the parent class?
 };
 
 #endif // PLAYERNETWORK_H

f7e8b9d11a767b3189a039eeb191682f49b18471
Christiaan Wagner <u04503237@tuks.co.za>
2020-09-27 18:11:07 +0200

    Added declarations in .h and .cpp files. Inheritace should be implemented. Enums and external classes should be added.

--- RubberContractBridge/main.cpp
+++ RubberContractBridge/main.cpp
@@ -1,12 +1,15 @@
 #include "mainwindow.h"
 #include "clientgui.h"
 #include "servergui.h"
+#include "playernetwork.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
-    QApplication rcbApplication(argc, argv);
-    MainWindow entryWindow;
-    entryWindow.show();
-    return rcbApplication.exec();
+    PlayerNetwork testPlayerNetowerk;
+
+//    QApplication rcbApplication(argc, argv);
+//    MainWindow entryWindow;
+//    entryWindow.show();
+//    return rcbApplication.exec();
 }
--- RubberContractBridge/playernetwork.cpp
+++ RubberContractBridge/playernetwork.cpp
@@ -1,6 +1,113 @@
 #include "playernetwork.h"
 
-PlayerNetwork::PlayerNetwork(QObject *parent) : QObject(parent)
+PlayerNetwork::PlayerNetwork(QObject *parent, QString playerName, QTcpSocket *clientSoc) : QObject(parent)
+{
+    // Once ingerited, init the playerName.
+    PlayerNetwork::clientSoc = clientSoc;
+
+}
+
+PlayerNetwork::~PlayerNetwork()
+{
+
+}
+
+void PlayerNetwork::notifyBidTurn()
+{
+
+}
+
+void PlayerNetwork::notifyMoveTurn()
+{
+
+}
+
+void PlayerNetwork::updateGameState(PlayerNetwork::PlayerGameState gameState)
+{
+
+}
+
+void PlayerNetwork::notifyBidRejected(QString reason)
+{
+
+}
+
+void PlayerNetwork::notifyMoveRejected(QString reason)
+{
+
+}
+
+void PlayerNetwork::message(QString source, QString msg)
+{
+
+}
+
+void PlayerNetwork::gameTerminated(QString reason)
+{
+
+}
+
+QString PlayerNetwork::getPlayerName() const
+{
+
+}
+
+PlayerNetwork::PlayerPosition PlayerNetwork::getPosition() const
+{
+
+}
+
+PlayerNetwork::Team PlayerNetwork::getTeam() const
+{
+
+}
+
+void PlayerNetwork::setPosition(PlayerNetwork::PlayerPosition position)
+{
+
+}
+
+QString PlayerNetwork::toString() const
+{
+
+}
+
+void PlayerNetwork::rxAll()
+{
+
+}
+
+void PlayerNetwork::pingClient()
+{
+
+}
+
+void PlayerNetwork::txAll(QJsonObject data)
+{
+
+}
+
+void PlayerNetwork::rxBidSelected(QJsonObject bidObj)
+{
+
+}
+
+void PlayerNetwork::rxMoveSelected(QJsonObject moveObj)
+{
+
+}
+
+void PlayerNetwork::rxMessage(QJsonObject msgObj)
+{
+
+}
+
+void PlayerNetwork::rxPingReturn()
+{
+
+}
+
+void PlayerNetwork::internalClientDisconnected()
 {
 
 }

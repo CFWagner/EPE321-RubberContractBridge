f10b8349011f44460d18f5fea4db49ae6ef4bf13
Christopher <u18072918@tuks.co.za>
2020-09-27 22:07:02 +0200

    Added BidCall, CardSuit, GameEvent, GamePhase, PlayerPosition and Team enums

--- RubberContractBridge/enumerations/CardRank.h
+++ RubberContractBridge/enumerations/CardRank.h
@@ -2,18 +2,20 @@
 #define CARDRANK_H
 
 // Represents rank of playing card. Integers are used to represent the value of the rank.
-enum CardRank {ACE = 1,
-              TWO = 2,
-              THREE = 3,
-              FOUR = 4,
-              FIVE = 5,
-              SIX = 6,
-              SEVEN = 7,
-              EIGHT = 8,
-              NINE = 9,
-              TEN = 10,
-              JACK = 11,
-              QUEEN = 12,
-              KING = 13};
+enum CardRank {
+    ACE = 1,
+    TWO = 2,
+    THREE = 3,
+    FOUR = 4,
+    FIVE = 5,
+    SIX = 6,
+    SEVEN = 7,
+    EIGHT = 8,
+    NINE = 9,
+    TEN = 10,
+    JACK = 11,
+    QUEEN = 12,
+    KING = 13
+};
 
 #endif // CARDRANK_H
---
+++ RubberContractBridge/enumerations/PlayerPosition.h
@@ -0,0 +1,12 @@
+#ifndef PLAYERPOSITION_H
+#define PLAYERPOSITION_H
+
+// Represents the position of a player in the game
+enum PlayerPosition{
+    N, // North
+    S, // South
+    E, // East
+    W // West
+};
+
+#endif // PLAYERPOSITION_H

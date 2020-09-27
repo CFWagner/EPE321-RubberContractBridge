688fe7c714f204233d3c92060c2f040f6a2b4613
Christiaan Wagner <u04503237@tuks.co.za>
2020-09-27 17:03:08 +0200

    Added declarations in .h and .cpp files.

--- RubberContractBridge/servernetwork.cpp
+++ RubberContractBridge/servernetwork.cpp
@@ -1,6 +1,47 @@
 #include "servernetwork.h"
 
-ServerNetwork::ServerNetwork(QObject *parent) : QObject(parent)
+ServerNetwork::ServerNetwork(QObject *parent, QString nameOfAI) : QObject(parent)
+{
+    ServerNetwork::nameOfAI = nameOfAI;
+    tcpServer = nullptr;
+}
+
+ServerNetwork::~ServerNetwork()
 {
 
+}
+
+QTcpSocket *ServerNetwork::getPlayerSoc(QString playerName) const
+{
+    return nullptr;
+}
+
+void ServerNetwork::setPassword(QString password)
+{
+
+}
+
+void ServerNetwork::initServer(QHostAddress ip)
+{
+
+}
+
+void ServerNetwork::stopListening()
+{
+
+}
+
+void ServerNetwork::connectClient()
+{
+
+}
+
+void ServerNetwork::disconnectClient()
+{
+
+}
+
+QString ServerNetwork::validateLogin(QString playerName, QString password)
+{
+    return QString("Temp");
 }

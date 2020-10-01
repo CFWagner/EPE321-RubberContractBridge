47c7d964a49dd32fb7fc1e2c71613faeadad6fb1
Christopher <u18072918@tuks.co.za>
2020-10-01 21:20:38 +0200

    Partially implemented ServerGameState and PlayerGameState classes.

--- RubberContractBridge/game-server/playergamestate.cpp
+++ RubberContractBridge/game-server/playergamestate.cpp
@@ -1,6 +1,33 @@
 #include "playergamestate.h"
 
-PlayerGameState::PlayerGameState()
+PlayerGameState::PlayerGameState() {}
+
+GameEvent PlayerGameState::getEvent()
+{
+    return gameEvent;
+}
+
+CardSet PlayerGameState::getPlayerHand()
+{
+    return playerHand;
+}
+
+CardSet PlayerGameState::getDummyHand()
+{
+    return dummyHand;
+}
+
+QString PlayerGameState::getPlayerName(PlayerPosition position)
+{
+    return playerPositions.value(position);
+}
+
+void PlayerGameState::read(const QJsonObject &json)
+{
+
+}
+
+void PlayerGameState::write(QJsonObject &json) const
 {
 
 }
--- RubberContractBridge/game-server/servergamestate.cpp
+++ RubberContractBridge/game-server/servergamestate.cpp
@@ -1,6 +1,54 @@
 #include "servergamestate.h"
 
-ServerGameState::ServerGameState()
+ServerGameState::ServerGameState() {}
+
+void ServerGameState::initialize()
+{
+
+}
+
+void ServerGameState::updateBidState(Bid bid)
+{
+
+}
+
+void ServerGameState::updatePlayState(PlayerPosition player, Card card)
+{
+
+}
+
+void ServerGameState::updatePhase(GamePhase phase)
+{
+
+}
+
+void ServerGameState::setDealer(PlayerPosition player)
+{
+    this->dealer = dealer;
+}
+
+void ServerGameState::setDeclarer(PlayerPosition player)
+{
+    this->declarer = declarer;
+}
+
+CardSet ServerGameState::getDeck()
+{
+    return deck;
+}
+
+PlayerGameState ServerGameState::getPlayerGameState()
+{
+    // Create player game state here
+    return PlayerGameState();
+}
+
+void ServerGameState::read(const QJsonObject &json)
+{
+
+}
+
+void ServerGameState::write(QJsonObject &json) const
 {
 
 }

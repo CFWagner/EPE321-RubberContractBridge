4a6a659fe62f5cc37d3e8f6ace88fbf230f514e0
Christopher <u18072918@tuks.co.za>
2020-10-05 23:56:59 +0200

    Create ServerGameState unit test skeleton.

---
+++ RubberContractBridge/unit-tests/game-server/testservergamestate.cpp
@@ -0,0 +1,6 @@
+#include "testservergamestate.h"
+
+TestServerGameState::TestServerGameState(QObject *parent) : QObject(parent)
+{
+
+}
---
+++ RubberContractBridge/unit-tests/game-server/testservergamestate.h
@@ -0,0 +1,16 @@
+#ifndef TESTSERVERGAMESTATE_H
+#define TESTSERVERGAMESTATE_H
+
+#include <QObject>
+
+class TestServerGameState : public QObject
+{
+    Q_OBJECT
+public:
+    explicit TestServerGameState(QObject *parent = nullptr);
+
+signals:
+
+};
+
+#endif // TESTSERVERGAMESTATE_H

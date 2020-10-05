eac6230dedb4b8839b90fea07fb11f29d44fd930
Christopher <u18072918@tuks.co.za>
2020-10-05 20:39:15 +0200

    Corrected undefined reference in CardSet header file. Cleaned up main.

--- RubberContractBridge/main.cpp
+++ RubberContractBridge/main.cpp
@@ -1,17 +1,10 @@
 #include "gui/mainwindow.h"
 #include <QApplication>
-//Unit test includes
-#include "unit-tests/game-server/testcard.h"
-#include "unit-tests/game-server/testbid.h"
-#include "unit-tests/game-server/testcardset.h"
 
 int main(int argc, char *argv[])
 {
-//    QApplication rcbApplication(argc, argv);
-//    MainWindow entryWindow;
-//    entryWindow.show();
-//    return rcbApplication.exec();
-    //Unit tests
-    TestCardSet testCardSet;
-    QTest::qExec(&testCardSet);
+    QApplication rcbApplication(argc, argv);
+    MainWindow entryWindow;
+    entryWindow.show();
+    return rcbApplication.exec();
 }

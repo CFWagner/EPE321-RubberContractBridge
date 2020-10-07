3b79bdaee89794cedf4a80fa2d80e77acc1e0943
Christiaan Wagner <u04503237@tuks.co.za>
2020-10-07 04:43:23 +0200

    Swap main files so that unit test main is the main.cpp file.

--- RubberContractBridge/main.cpp
+++ RubberContractBridge/main.cpp
@@ -1,10 +1,23 @@
-#include "mainwindow.h"
+// This main is solely used for testing
 #include <QApplication>
+#include "unit-tests/network/testservernetwork.h"
+#include "unit-tests/network/testplayernetwork.h"
+#include "unit-tests/network/testclientnetwork.h"
 
 int main(int argc, char *argv[])
 {
+    // QApplication is nescessary so that QSignalSpy.wait() can be used in the tests.
     QApplication rcbApplication(argc, argv);
-    MainWindow entryWindow;
-    entryWindow.show();
+
+    // Test the main class
+    testServerNetwork testServerNet;
+    QTest::qExec(&testServerNet);
+    
+    testPlayerNetwork testPlayerNet;
+    QTest::qExec(&testPlayerNet);
+    
+    testClientNetwork testClientNet;
+    QTest::qExec(&testClientNet);
+
     return rcbApplication.exec();
 }

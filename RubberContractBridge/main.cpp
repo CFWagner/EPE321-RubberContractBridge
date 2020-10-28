784a9597310cfa37d1ecfa2fabeeb2efa72cfcfe
Christopher <u18072918@tuks.co.za>
2020-10-28 14:13:10 +0200

    Clean up

--- RubberContractBridge/main.cpp
+++ RubberContractBridge/main.cpp
@@ -1,39 +1,10 @@
 #include "gui/entrywindow.h"
-#include "gui/bidwindow.h"
-#include "gui/bridgewindow.h"
 #include <QApplication>
-#include "unit-tests/game-server/testbid.h"
-#include "unit-tests/game-server/testcard.h"
-#include "unit-tests/game-server/testcardset.h"
-#include "unit-tests/game-server/testgameserver.h"
-#include "unit-tests/game-server/testplayergamestate.h"
-#include "unit-tests/game-server/testscore.h"
-#include "unit-tests/game-server/testserver.h"
-#include "unit-tests/game-server/testservergamestate.h"
 
 int main(int argc, char *argv[])
 {
-    TestBid testBid;
-    QTest::qExec(&testBid);
-
-    TestCard testCard;
-    QTest::qExec(&testCard);
-
-    TestCardSet testCardset;
-    QTest::qExec(&testCardset);
-
-    TestScore testScore;
-    QTest::qExec(&testScore);
-
-    TestPlayerGameState testPlayerGameState;
-    QTest::qExec(&testPlayerGameState);
-
-    TestServerGameState testServerGameState;
-    QTest::qExec(&testServerGameState);
-
-    TestGameServer testGameServer;
-    QTest::qExec(&testGameServer);
-
-    TestServer testServer;
-    QTest::qExec(&testServer);
+    QApplication rcbApplication(argc, argv);
+    EntryWindow entryWindow;
+    entryWindow.show();
+    return rcbApplication.exec();
 }

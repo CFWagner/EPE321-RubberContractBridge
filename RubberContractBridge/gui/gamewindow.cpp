c3d12dddc00f2b0b167204bfa876369b1d8882fc
Hanro vdW <u18141235@tuks.co.za>
2020-11-03 16:25:01 +0200

    Lobby chat implemented

--- RubberContractBridge/gui/gamewindow.cpp
+++ RubberContractBridge/gui/gamewindow.cpp
@@ -12,6 +12,9 @@ GameWindow::GameWindow(ClientNetwork *clientNetwork, QWidget *parent) : QWidget(
     connect(clientNetwork,&ClientNetwork::notifyMoveTurn, this,&GameWindow::playerTurnMove);
     connect(clientNetwork,&ClientNetwork::notifyBidRejected, this,&GameWindow::bidRejected);
     connect(clientNetwork,&ClientNetwork::notifyMoveRejected, this,&GameWindow::moveRejected);
+    connect(clientNetwork,&ClientNetwork::messageReceived, this,&GameWindow::messageReceived);
+    connect(this,&GameWindow::sendMessage, clientNetwork,&ClientNetwork::txMessage);
+    connect(this,&GameWindow::bidAction, clientNetwork,&ClientNetwork::txBidSelected);
     connect(this,&GameWindow::bidAction, clientNetwork,&ClientNetwork::txBidSelected);
     connect(this,&GameWindow::cardAction, clientNetwork,&ClientNetwork::txMoveSelected);
     setupWindow();
@@ -829,6 +832,12 @@ void GameWindow::on_button_exit_clicked()
     this->close();
 }
 
+void GameWindow::messageReceived(QString source, QString msg)
+{
+    QString reMessage =" {" + source + "}: " + msg;
+    ui->messagePanel->appendPlainText(reMessage);
+}
+
 void GameWindow::bidRejected(QString reason)
 {
     playerMayBid = true;
@@ -851,4 +860,10 @@ void GameWindow::setupWindow()
     this->setFixedSize(1920,1080);
     this->setWindowTitle ("Rubber Contract Bridge");
     setWindowIcon(QIcon(":/resources/guiResources/cards/ace_spades.png"));
+}
+
+void GameWindow::on_messagerB_clicked()
+{
+    emit sendMessage(ui->messageEdit->text());
+    ui->messageEdit->clear();
 }
